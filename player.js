09b0488238139a3102902ad247d6ef93ee461b66
Worstik11 <sadistik22@gmail.com>
2024-06-20 15:03:25 +0300

    Update player.js

--- player.js
+++ player.js
@@ -4,12 +4,12 @@ const { queueNames } = require("./commands/play");
 
 function initializePlayer(client) {
     const nodes = [
-      {
-            host: "lava-v4.ajieblogs.eu.org",
-            password: "https://dsc.gg/ajidevserver",
-            port: 443,
-            secure: true
-        },
+   {
+    host : "37.114.42.191",
+    port : 6767,
+    password : "danteisnttaken",
+    secure : false
+  },
     ];
 
     client.riffy = new Riffy(client, nodes, {
